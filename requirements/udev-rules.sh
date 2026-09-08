df85f9a35ad2f1734613f8e7f68270297ee65da5
SeonilChoi <seonilchoi98@gmail.com>
2026-09-08 14:18:17 +0900

    udev-rules.sh updated

--- requirements/udev-rules.sh
+++ requirements/udev-rules.sh
@@ -2,8 +2,9 @@
 set -euo pipefail
 
 cat << 'EOF' | sudo tee /etc/udev/rules.d/99-humanoid-controller.rules
-KERNEL=="ttyUSB*", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6001", MODE="0666", SYMLINK+="unitree"
-KERNEL=="ttyUSB*", ATTRS{idVendor}=="2639", MODE="0666", SYMLINK+="xsens"
+KERNEL=="ttyACM*", ATTRS{idVendor}=="16d0", ATTRS{idProduct}=="117e", MODE="0666", SYMLINK+="CANable"
+KERNEL=="ttyUSB*", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6014", MODE="0666", SYMLINK+="Unitree"
+KERNEL=="ttyUSB*", ATTRS{idVendor}=="2639", MODE="0666", SYMLINK+="Xsens"
 ACTION=="add", SUBSYSTEM=="usb-serial", ATTR{latency_timer}="1"
 EOF
 
